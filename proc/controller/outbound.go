316a538f512c34242533823c126a5c4e5e554363
shynome <shynome@gmail.com>
2019-05-09 22:54:03 +0800

    只要传入 shareStr 就行了

--- proc/controller/outbound.go
+++ proc/controller/outbound.go
@@ -3,13 +3,21 @@ package controller
 import (
 	"context"
 
-	"v2ray.com/core"
-
+	apiproxyProc "github.com/shynome/v2ray-apiproxy/proc"
+	"github.com/shynome/v2ray-apiproxy/vnext"
 	"v2ray.com/core/app/proxyman/command"
 )
 
 // Add new v2ray apiproxy
-func (ctrol *Controller) Add(tag, outboundConfig *core.OutboundHandlerConfig) (port uint16, err error) {
+func (ctrol *Controller) Add(shareStr string) (port uint16, err error) {
+
+	vn, err := vnext.New(shareStr)
+	if err != nil {
+		return
+	}
+
+	tag := apiproxyProc.TagAPIPorxy + string(ctrol.Info.PortCursor)
+	outboundConfig := vn.NewVMessOutboundConfig(tag)
 
 	hsClient, err := ctrol.gethsClient()
 	if err != nil {
@@ -21,6 +29,10 @@ func (ctrol *Controller) Add(tag, outboundConfig *core.OutboundHandlerConfig) (p
 	}
 
 	_, err = hsClient.AddOutbound(context.Background(), req)
+
+	if err == nil {
+		ctrol.Info.PortCursor++
+	}
 
 	return
 
